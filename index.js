d39b63fd92ad23824e641e16a31ee782c6529b25
Denis Trotckii <Denis.Trotckii@loni.usc.edu>
2016-10-26 15:49:08 -0700

    Connected to firebase.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var firebase = require("firebase");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,4 +18,23 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var config = {
+apiKey: "AIzaSyDH8qA0uF6b8AfNeaB7oz_T57_y7PXurqo",
+    authDomain: "russian-soccer.firebaseapp.com",
+    databaseURL: "https://russian-soccer.firebaseio.com",
+    storageBucket: "russian-soccer.appspot.com",
+    messagingSenderId: "476867980667"
+};
+var mainApp = firebase.initializeApp(config);
 
+var database = firebase.database();
+
+firebase.database().ref('/players/player').once('value').then(function(snapshot) {
+  var username = snapshot.val().FirstName;
+  console.log('Player First Name is ', username);
+});
+
+var commentsRef = firebase.database().ref('players');
+commentsRef.on('child_added', function(data) {
+  console.log('Player ', data.val());
+});

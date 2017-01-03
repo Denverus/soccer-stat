79cd9fcd38f0841c437eeb47eee164ea40ef663f
Denis Trotsky <denveru@gmail.com>
2017-01-02 21:52:12 -0800

    Removed unused code.

--- index.js
+++ index.js
@@ -313,14 +313,3 @@ app.get('/test', function (request, response) {
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-
-var config = {
-    apiKey: "AIzaSyDH8qA0uF6b8AfNeaB7oz_T57_y7PXurqo",
-    authDomain: "russian-soccer.firebaseapp.com",
-    databaseURL: "https://russian-soccer.firebaseio.com",
-    storageBucket: "russian-soccer.appspot.com",
-    messagingSenderId: "476867980667"
-};
-
-
-gameTool.copyEvents();
